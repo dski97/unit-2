9e4c1df1e51354c2cca610fdba245d6f48057d4e
DOMINIC CWAL <dcwalinski97@gmai.com>
2023-02-06 21:27:07 -0500

    Activity 5: Leaflet Tutorials and Lab Dataset
    add the required files for spatial data and fix code

--- js/main.js
+++ js/main.js
@@ -1,1 +1,62 @@
-// Add all scripts to the JS folder
+/* Map of GeoJSON data from countriesInflation that shows consumer inflation prices from 1985-2020 in 5 year intervals. */
+//declare map var in global scope
+var map;
+//function to instantiate the Leaflet map
+function createMap(){
+    //create the map
+    map = L.map('map', {
+        center: [20, 0],
+        zoom: 2.5
+    });
+
+    //add OSM base tilelayer
+    L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+        maxZoom: 16
+    }).addTo(map);
+
+    //call getData function
+    getData();
+};
+
+//function to attach popups to each mapped feature
+function onEachFeature(feature, layer) {
+    //no property named popupContent; instead, create html string with all properties
+    var popupContent = "";
+    if (feature.properties) {
+        //loop to add feature property names and values to html string
+        for (var property in feature.properties){
+            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+        }
+        layer.bindPopup(popupContent);
+    };
+};
+
+//function to retrieve the data and place it on the map
+function getData(){
+    //load the data
+    fetch("data/countriesInflation.geojson")
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(json){            
+            //create marker options
+            var geojsonMarkerOptions = {
+                radius: 8,
+                fillColor: "#ff7800",
+                color: "#000",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8
+            };
+            //create a Leaflet GeoJSON layer and add it to the map
+            L.geoJson(json, {
+                pointToLayer: function (feature, latlng){
+                    return L.circleMarker(latlng, geojsonMarkerOptions);
+                },
+                onEachFeature: onEachFeature
+            }).addTo(map);
+        });
+};
+
+document.addEventListener('DOMContentLoaded',createMap)
